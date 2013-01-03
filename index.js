f58aa16f27d2246461b9b875bf7efb030cd7c4c6
Thomas Buckley-Houston <tom@tombh.co.uk>
2013-01-03 23:29:08 +0000

    Use express example from integration tests

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-// Load the http module to create an http server.
-var http = require('http');
-
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+var express = require('express');
+var app = express();
+app.get('/', function(req, res){
+  res.send('Hello World');
 });
-
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
-
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");
+var port = process.env.PORT;
+app.listen(port);
+console.log('Listening on port ' + port);
